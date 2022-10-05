b001ac52c052838f5fdfa8c39b0a8fd9d1340bdc
fullstackdev-pro <imomovismoi911@gmail.com>
2022-10-05 19:27:45 +0500

    responsive qilindi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [vaqt, setVaqt] = useState(0);
   const [radius, setRadius] = useState(0);
-  const [aylson, setAylson] = useState(0);
+  // const [aylson, setAylson] = useState(0);
   const [tezlik, setTezlik] = useState(0);
 
   const [davr, setDavr] = useState(0);
@@ -19,9 +19,9 @@ function App() {
     setRadius(event.target.value);
   };
 
-  const handleChangeAylson = (event) => {
-    setAylson(event.target.value);
-  };
+  // const handleChangeAylson = (event) => {
+  //   setAylson(event.target.value);
+  // };
 
   const handleChangeTezlik = (event) => {
     setTezlik(event.target.value);
@@ -42,64 +42,63 @@ function App() {
   }
 
   return (
-    <div className="flex justify-between my-10 mx-16 h-[85vh] ">
+    <div className="xl:flex xl:justify-between xl:my-10 xl:mx-16 overflow-x-hidden">
       <div
-        className="p-16 w-[60%] h-[100%] border-[1px] border-r-0 border-black 
-    flex justify-center items-center rounded-tl-lg rounded-bl-lg"
+        className="xl:p-16 xl:w-[60%] xl:h-[100%] border-[1px] xl:border-r-0 border-black 
+        flex justify-center items-center rounded-tr-lg xl:rounded-tr-none rounded-tl-lg xl:rounded-bl-lg"
       >
         <div
-          className="inline-block rounded-full w-[25rem] h-[25rem] animate-spin"
+          className="xl:w-[25rem] xl:h-[25rem] animate-spin "
           style={{ animation: `spin ${chastota}s linear infinite` }}
         >
-          <div
-            className="bg-[url('https://pngimg.com/uploads/spinner/spinner_PNG10.png')] 
-        bg-cover bg-no-repeat w-[92%] h-[85%]"
-          ></div>
+          <img src="https://pngimg.com/uploads/spinner/spinner_PNG56.png" alt="spinner" 
+          className="w-full h-full "/>
         </div>
       </div>
 
-      <div className="w-[40%] h-full bg-blue-50 border-[1px] border-black text-center rounded-tr-lg rounded-br-lg">
+      <div className="xl:w-[40%] xl:h-full bg-blue-50 border-[1px] border-black text-center 
+      xl:rounded-tr-lg rounded-br-lg pb-12">
         <div className="border-b-[1px] border-black">
           <div className="py-8 text-left pl-12">
-            <label htmlFor="davr">Vaqt, sekund</label>
+            <label htmlFor="davr">Vaqt</label>
             <input
               type="number"
               id="davr"
-              className="ml-8 pl-4 rounded-sm right"
+              className="ml-[4.5rem] pl-4 rounded-sm right w-32"
               onChange={handleChangeVaqt}
               required
-            />
+            /> sekund
             <br /> <br />
-            <label htmlFor="radius">Radius, sm</label>
+            <label htmlFor="radius">Radius </label>
             <input
               type="number"
               id="radius"
-              className="ml-[2.9rem] pl-4 rounded-sm"
+              className="ml-[3.4rem] pl-4 rounded-sm w-32"
               onChange={handleChangeRadius}
               required
-            />
+            /> sm
             <br /> <br />
-            <label htmlFor="aylson">Aylana soni, ta</label>
+            <label htmlFor="aylson">Aylana soni </label>
             <input
               type="number"
               id="aylson"
-              className="ml-[1.3rem] pl-4 rounded-sm"
-              onChange={handleChangeAylson}
+              className="ml-[1.4rem] pl-4 rounded-sm w-32"
+              // onChange={handleChangeAylson}
               required
-            />
+            /> ta
             <br /> <br />
-            <label htmlFor="tezlik">Tezlik, N</label>
+            <label htmlFor="tezlik">Tezlik </label>
             <input
               type="number"
               id="tezlik"
-              className="ml-[3.9rem] pl-4 rounded-sm"
+              className="ml-[3.9rem] pl-4 rounded-sm w-32"
               onChange={handleChangeTezlik}
               required
-            />
+            /> N
             <br />
             <br />
             <button
-              className="border-2 py-1 px-8 ml-[7rem] rounded-lg "
+              className="border-2 py-1 px-12 ml-[6.3rem] rounded-lg text-white text-xl bg-green-400"
               onClick={() => {
                 Started();
               }}
@@ -108,16 +107,17 @@ function App() {
             </button>
           </div>
         </div>
-        <div className="text-left pl-12">
+        
+        <div className="text-left pl-12 pb-[0.45rem]">
           <div className="mt-8">
-            Davr: <span className="pl-[5.6rem]">{davr} sekund.</span>
+            Davr: <span className="pl-[5.65rem]">{davr} sekund.</span>
           </div>
           <div className="mt-4">
             Chastota: <span className="pl-[3.6rem]">{chastota} Hz.</span>
           </div>
           <div className="mt-4">
             O'rtacha tezlik:{" "}
-            <span className="pl-[1.2rem]">{orttezlik} m/s.</span>
+            <span className="pl-[1.3rem]">{orttezlik} m/s.</span>
           </div>
           <div className="mt-4">
             Burchak tezlik:{" "}
